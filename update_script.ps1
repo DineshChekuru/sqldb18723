4daf6d03ab1efec2ff339f5a80145606456722db
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-07-20 12:53:52 +0530

    Update update_script.ps1

--- update_script.ps1
+++ update_script.ps1
@@ -5,10 +5,10 @@ $UpdatedConfigContent = Get-Content -Path $UpdatedConfigPath | ConvertFrom-Json
 # Set the updated environment variables from the updated configuration
 $Env:workspaceName = $UpdatedConfigContent.parameters.workspaceName.value
 $Env:AzureDataLakeStorage1_accountKey = $UpdatedConfigContent.parameters.AzureDataLakeStorage1_accountKey.value
-$Env:syn1737_WorkspaceDefaultSqlServer_connectionString = $UpdatedConfigContent.parameters.syn1737-WorkspaceDefaultSqlServer_connectionString.value
+$Env:syn1737_WorkspaceDefaultSqlServer_connectionString = $UpdatedConfigContent.parameters.syn1737_WorkspaceDefaultSqlServer_connectionString.value
 $Env:AzureDataLakeStorage1_properties_typeProperties_url = $UpdatedConfigContent.parameters.AzureDataLakeStorage1_properties_typeProperties_url.value
 $Env:AzureKeyVault1_properties_typeProperties_baseUrl = $UpdatedConfigContent.parameters.AzureKeyVault1_properties_typeProperties_baseUrl.value
-$Env:syn1737_WorkspaceDefaultStorage_properties_typeProperties_url = $UpdatedConfigContent.parameters.syn1737-WorkspaceDefaultStorage_properties_typeProperties_url.value
+$Env:syn1737_WorkspaceDefaultStorage_properties_typeProperties_url = $UpdatedConfigContent.parameters.syn1737_WorkspaceDefaultStorage_properties_typeProperties_url.value
 
 # Rerun the workflow with the updated configuration values
 Invoke-Expression -Command "Your command to rerun the workflow with the new environment variables"
