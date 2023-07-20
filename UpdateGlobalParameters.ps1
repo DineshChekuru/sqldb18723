bf6d2e9219219658b9735fe6deeda5ecf4ecb474
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-07-20 18:13:28 +0530

    Update UpdateGlobalParameters.ps1

--- UpdateGlobalParameters.ps1
+++ UpdateGlobalParameters.ps1
@@ -2,20 +2,20 @@ param
 (
     [parameter(Mandatory = $true)] [String] $globalParametersFilePath,
     [parameter(Mandatory = $true)] [String] $resourceGroupName,
-    [parameter(Mandatory = $true)] [String] $dataFactoryName
+    [parameter(Mandatory = $true)] [String] $workspaceName
 )
 Import-Module Az.Synapse
-$newGlobalParameters = New-Object 'system.collections.generic.dictionary[string,Microsoft.Azure.Management.DataFactory.Models.GlobalParameterSpecification]'
+$newGlobalParameters = New-Object 'system.collections.generic.dictionary[string,Microsoft.Azure.Management.Synapse.Models.GlobalParameterSpecification]'
 Write-Host "Getting global parameters JSON from: " $globalParametersFilePath
 $globalParametersJson = Get-Content $globalParametersFilePath
 Write-Host "Parsing JSON..."
 $globalParametersObject = [Newtonsoft.Json.Linq.JObject]::Parse($globalParametersJson)
 foreach ($gp in $globalParametersObject.GetEnumerator()) {
     Write-Host "Adding global parameter:" $gp.Key
-    $globalParameterValue = $gp.Value.ToObject([Microsoft.Azure.Management.DataFactory.Models.GlobalParameterSpecification])
+    $globalParameterValue = $gp.Value.ToObject([Microsoft.Azure.Management.Synapse.Models.GlobalParameterSpecification])
     $newGlobalParameters.Add($gp.Key, $globalParameterValue)
 }
-$dataFactory = Get-AzDataFactoryV2 -ResourceGroupName $resourceGroupName -Name $dataFactoryName
+$dataFactory = Get-AzSynapseWorkspace -ResourceGroupName $resourceGroupName -Name $dataFactoryName
 $dataFactory.GlobalParameters = $newGlobalParameters
 Write-Host "Updating" $newGlobalParameters.Count "global parameters."
 Set-AzDataFactoryV2 -InputObject $dataFactory -Force
