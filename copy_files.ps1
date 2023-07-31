09441bd5fb5792817a2e4d6cbaf2fbb7e292c026
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-07-31 23:26:29 +0530

    Update copy_files.ps1

--- copy_files.ps1
+++ copy_files.ps1
@@ -2,8 +2,19 @@
 $sourceFolder = ".\dev"
 $destinationFolder = ".\prod"
 
+Write-Host "Source Folder: $sourceFolder"
+Write-Host "Destination Folder: $destinationFolder"
+
 # Navigate to the source folder
 Set-Location -Path $sourceFolder
 
+# List the files in the source folder (debugging)
+Write-Host "Files in source folder:"
+Get-ChildItem -Path $sourceFolder
+
 # Copy files from the source folder to the destination folder
 Copy-Item -Path $sourceFolder\* -Destination $destinationFolder -Recurse
+
+# List the files in the destination folder after copying (debugging)
+Write-Host "Files in destination folder:"
+Get-ChildItem -Path $destinationFolder -Recurse
