80c2576181b78f6948f86693aa6c073d158e37a7
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-07-31 23:10:08 +0530

    Update copy_files.ps1

--- copy_files.ps1
+++ copy_files.ps1
@@ -1,16 +1,6 @@
-# PowerShell script to copy files from the "dev" folder to the "prod" folder
-$sourcePath = ".\dev"
-$destinationPath = ".\prod"
+# Set source and destination folder paths
+$sourceFolder = "path/to/source/folder"
+$destinationFolder = "path/to/destination/folder"
 
-# Create the destination folder if it doesn't exist
-if (-Not (Test-Path -Path $destinationPath -PathType Container)) {
-    New-Item -Path $destinationPath -ItemType Directory | Out-Null
-}
-
-# Get a list of files in the source folder
-$files = Get-ChildItem -Path $sourcePath -File
-
-foreach ($file in $files) {
-    $destinationFilePath = Join-Path -Path $destinationPath -ChildPath $file.Name
-    Copy-Item -Path $file.FullName -Destination $destinationFilePath -Force
-}
+# Copy files from the source folder to the destination folder
+Copy-Item -Path $sourceFolder\* -Destination $destinationFolder -Recurse
