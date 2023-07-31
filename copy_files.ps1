a2bcacdcbf6d368a63669a72ebca3fd0f877615a
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-07-31 23:14:40 +0530

    Update copy_files.ps1

--- copy_files.ps1
+++ copy_files.ps1
@@ -1,6 +1,6 @@
 # Set source and destination folder paths
-$sourceFolder = "path/to/source/folder"
-$destinationFolder = "path/to/destination/folder"
+$sourceFolder = ".\dev"
+$destinationFolder = ".\prod"
 
 # Copy files from the source folder to the destination folder
 Copy-Item -Path $sourceFolder\* -Destination $destinationFolder -Recurse
