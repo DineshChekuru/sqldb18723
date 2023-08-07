c4553754976390e7f05ca37fc67d2a258d57717d
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-08-07 22:57:36 +0530

    Update config.ps1

--- config.ps1
+++ config.ps1
@@ -1,3 +1,4 @@
+cd sqldb18723
 $E = [System.Collections.ArrayList]@()
             $E = Get-Content -Path ./dev/notebook/CDS_Raw_daily.json
             $E = $E -replace "https://syn-tccc-mad-use2-dev-01.dev.azuresynapse.net/livyApi/versions/2019-11-01-preview/sparkPools/apsmaddev01", "https://syn-tccc-mad-use20-dev-01.dev.azuresynapse.net/livyApi/versions/2019-11-01-preview/sparkPool/apsmaddev01"
