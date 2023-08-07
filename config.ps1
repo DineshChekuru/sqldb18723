3707f4d2eedf817ecef962ab2eb850578f45adc5
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-08-07 22:55:46 +0530

    Create config.ps1

---
+++ config.ps1
@@ -0,0 +1,5 @@
+$E = [System.Collections.ArrayList]@()
+            $E = Get-Content -Path ./dev/notebook/CDS_Raw_daily.json
+            $E = $E -replace "https://syn-tccc-mad-use2-dev-01.dev.azuresynapse.net/livyApi/versions/2019-11-01-preview/sparkPools/apsmaddev01", "https://syn-tccc-mad-use20-dev-01.dev.azuresynapse.net/livyApi/versions/2019-11-01-preview/sparkPool/apsmaddev01"
+            $E = $E -replace "apsmaddev01", "prod0198"
+            $E | Set-Content -Path ./dev/notebook/CDS_Raw_daily.json
