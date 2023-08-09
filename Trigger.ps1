d71b204a2c2a00aee29d30ef1a3cb649ca06c1d3
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-08-09 13:54:41 +0530

    Add files via upload

---
+++ Trigger.ps1
@@ -0,0 +1,9 @@
+﻿$synapsetrigger = az synapse trigger list --workspace-name synprod | ConvertFrom-Json
+              foreach ($syn in $synapsetrigger) {
+              if($syn.name -eq "Triggerdev" -or $syn.name -eq "Triggersp"){
+              az synapse trigger start --name $syn.name --workspace-name synprod
+              }
+              else{
+              az synapse trigger stop --name $syn.name --workspace-name synprod
+              }
+              }
