48e80279e73f33e90b257a9308ca1aa4c212eda4
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-08-09 14:07:58 +0530

    Create Trigger.ps1

---
+++ Trigger.ps1
@@ -0,0 +1,6 @@
+﻿$synapsetrigger = az synapse trigger list --workspace-name synprod | ConvertFrom-Json
+              foreach ($syn in $synapsetrigger) {
+              if($syn.name -eq "Trigger"){
+              az synapse trigger start --name $syn.name --workspace-name synprod
+              }
+              
