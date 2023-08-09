86bf3aa5b8b2deef4126982bbcd036f36237e36c
DineshChekuru <67067731+DineshChekuru@users.noreply.github.com>
2023-08-09 14:19:21 +0530

    Update Trigger.ps1

--- Trigger.ps1
+++ Trigger.ps1
@@ -1,7 +1,7 @@
 ﻿$synapsetrigger = az synapse trigger list --workspace-name synprod | ConvertFrom-Json
               foreach ($syn in $synapsetrigger) {
               if($syn.name -eq "Trigger"){
-              az synapse trigger start --name $syn.name --workspace-name synprod
+              az synapse trigger stop --name $syn.name --workspace-name synprod
               }
               }
               
